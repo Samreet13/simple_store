31bd51d34f0807d1c8ebb6909a1978c840f55fa9
Samreet13 <skaur228@rrc.ca>
2026-03-02 12:02:18 -0600

    Init

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+require "csv"
+
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |row|
+  category = Category.find_or_create_by(name: row['category'])
+
+  Product.create!(
+    title: row['name'],
+    description: row['description'],
+    price: row['price'],
+    stock_quantity: row['stock quantity'],  # note the space!
+    category: category
+  )
+end
